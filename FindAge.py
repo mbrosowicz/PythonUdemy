ef6baaa1c7404a3aa8986c235bbb414866ccf540
Marcelo Brosowicz de Paulo <mbrosowicz@hotmail.com>
2018-04-19 16:05:17 -0300

    Indent

--- FindAge.py
+++ FindAge.py
@@ -1,6 +1,6 @@
 import datetime
 
-DOB = input("Enter your DOB:")
+DOB = input("Enter your DOB: ")
 CurrentYear = datetime.datetime.now().year
 Age = CurrentYear - int(DOB)
 print("Your age is {}".format(Age))
